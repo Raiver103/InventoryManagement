eb063c4e782c5b2eb11d70919707c8fd9db52b18
raiver <ilya.maksimovich.01@gmail.com>
2025-03-18 20:32:19 +0300

    fix

---
+++ InventoryManagement.Tests/SqlServerFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+
+namespace InventoryManagement.Tests
+{
+    public class SqlServerFixture : IAsyncLifetime
+    {
+        public MsSqlContainer DbContainer { get; } = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+            .WithPassword("Strong!Password@123")
+            .Build();
+
+        public string ConnectionString => DbContainer.GetConnectionString();
+
+        public async Task InitializeAsync()
+        {
+            await DbContainer.StartAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await DbContainer.DisposeAsync();
+        }
+    }
+}
